76f61583dc14b02d86d1c58bcaf167e19669633a
shota aoki <shotaaoki@mbp.local>
2021-02-05 13:16:08 +0900

    ストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,18 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all.order(created_at: :DESC)
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :text, :price, :category_id, :postage_id, :prefecture_id, :day_id, :image).merge(user_id: current_user.id)
+  end
 end
